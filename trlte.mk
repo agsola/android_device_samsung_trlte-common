8a514d60437132284e6a72bb8aae48fd2c1a1c2d
agsola <agsola@users.noreply.github.com>
2016-02-26 00:03:17 +0100

    Update trlte.mk
    Remove tab and white spaces.

--- trlte.mk
+++ trlte.mk
@@ -108,8 +108,8 @@ PRODUCT_PACKAGES += \
     Snap
     
 # Charger
-PRODUCT_PACKAGES += \		
-    charger_res_images		
+PRODUCT_PACKAGES += \
+    charger_res_images
 
 # Display
 PRODUCT_PACKAGES += \
